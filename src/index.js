97db8ecbb71177d6dd42b75f1f3ad1437b4be301
umahaea <eajazali.mahaboobbasha@pearson.com>
2018-04-23 17:18:26 -0600

    minor

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ var express = require('express')
 var app = express()
 
 app.use('/coverage',coverage.createHandler());
-app.listen(3000);
+app.listen(3000, () => console.log('Coverage app listening on port 3000!'));
